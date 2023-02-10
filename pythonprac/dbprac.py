ca76d7ecd994ba6771064cbcf6b38f926489bcef
dhtjddls <dhtjddls12@naver.com>
2023-02-10 13:24:56 +0900

    sparta prac

---
+++ pythonprac/dbprac.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.xzw0iic.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
---
+++ pythonprac/hello.py
@@ -0,0 +1,6 @@
+ages = [5, 10 , 13, 23, 25, 7]
+for a in ages:
+    if a > 19 :
+        print('성인입니다.')
+    else:
+        print('급식입니다.')
