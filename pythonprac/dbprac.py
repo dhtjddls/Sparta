a70bd689170c2d7a4d325bd8347ee498b47489ab
dhtjddls <dhtjddls12@naver.com>
2023-02-10 15:04:41 +0900

    url

--- pythonprac/dbprac.py
+++ pythonprac/dbprac.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.xzw0iic.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('url')
 db = client.dbsparta
