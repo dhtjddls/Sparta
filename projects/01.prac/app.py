ea054e5400da1ac9f9ba74186061cea274a4b930
dhtjddls <dhtjddls12@naver.com>
2023-02-14 12:53:11 +0900

    mars project

--- projects/01.prac/app.py
+++ projects/01.prac/app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
 
-@app.router('/mypage')
-def mypage():
-    return 'my page'
+
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
